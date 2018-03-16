09750467e5d328938a21b1d93da184f1a0e49aae
Quivo <37252176+quivop@users.noreply.github.com>
2018-03-16 16:06:21 -0700

    Modify urls.py to forward requests for 'fanarchive/' appropriately

--- archive/archive/urls.py
+++ archive/archive/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include # we need include, so add it here
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # forward requests for 'fanarchive/' links to the fanarchive app
+    path('fanarchive/', include('fanarchive.urls')), 
 ]
