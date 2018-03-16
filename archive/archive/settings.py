00c55407bc7a6405362a8744c7307b00bd212e55
Quivo <37252176+quivop@users.noreply.github.com>
2018-03-16 15:34:53 -0700

    Register Fanarchive app in site settings

--- archive/archive/settings.py
+++ archive/archive/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Registering the Fanarchive app
+    'fanarchive.apps.FanarchiveConfig'
 ]
 
 MIDDLEWARE = [
