1ada594f384cdc7670f22d77b4b1c94c3e1330b2
Quivo <37252176+quivop@users.noreply.github.com>
2018-03-16 16:13:08 -0700

    Add super-simple index view to views.py

--- archive/fanarchive/views.py
+++ archive/fanarchive/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+# import to enable super-simple index page
+from django.http import HttpResponse
+
+# super-simple index page
+def index(request):
+	return HttpResponse("Hello, cruel fanfic world. You are at the fanarchive index.")
