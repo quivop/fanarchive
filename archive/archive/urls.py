b418b8ca842950679010102c9ba475546e365557
Quivo <37252176+quivop@users.noreply.github.com>
2018-03-16 16:23:48 -0700

    Modify urls.py to set up redirect to fanarchive app
    This is set up so that "localhost:8000/" will redirect to "localhost:8000/fanarchive/".

--- archive/archive/urls.py
+++ archive/archive/urls.py
@@ -16,8 +16,18 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include # we need include, so add it here
 
+# Import RedirectView so redirecting our base url
+# to the fanarchive app will work
+from django.views.generic import RedirectView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # forward requests for 'fanarchive/' links to the fanarchive app
+
+    # forward requests for 'fanarchive/' links to urls.py 
+    # in the fanarchive app
     path('fanarchive/', include('fanarchive.urls')), 
+
+    # redirect the base url to the fanarchive app
+    path('', RedirectView.as_view(url='/fanarchive/')),
 ]
+
