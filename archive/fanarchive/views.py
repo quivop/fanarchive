b8c894b2e9f8932d148ea0b22a57e5f47c7c7f15
quivop <37252176+quivop@users.noreply.github.com>
2018-03-19 20:10:53 -0700

    Add dynamic new index page that lists recent works
    Modified index.jinja template as well as views.py

--- archive/fanarchive/views.py
+++ archive/fanarchive/views.py
@@ -1,12 +1,21 @@
 from django.shortcuts import render
 
-# import to enable super-simple index page
+# import to enable super-simple pages
 from django.http import HttpResponse
 
-# super-simple index page
+# import to enable DYNAMIC NEW index page
+from .models import Work
+
+# DYNAMIC NEW index page
 def index(request):
-	# now with Jinja2~
-	return render(request, 'fanarchive/index.jinja')
+	# get the five latest works
+	latest_work_list = Work.objects.order_by('-date_created')[:5]
+	
+	# pass latest works to a variable
+	the_works = {'latest_work_list': latest_work_list,}
+
+	# render latest works using index.jinja template and the latest_work_list variable
+	return render(request, 'fanarchive/index.jinja', the_works)
 
 # super simple detail page
 def detail(request, work_id):
