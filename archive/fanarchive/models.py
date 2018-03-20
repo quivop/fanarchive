87051150a5e7fc2a9d97cbe41f96b76aebdb4c65
quivop <37252176+quivop@users.noreply.github.com>
2018-03-19 18:26:24 -0700

    Add Work model to fanarchive app

--- archive/fanarchive/models.py
+++ archive/fanarchive/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+# Work model
+
+class Work(models.model):
+	# work title CharField with generous length allowance
+	work_title = models.CharField('work title', max_length=200)
+	# work summary TextField with max-length enforced only at view level, not on db or in the model
+	work_summary = models.TextField('work summary', max_length=500)
+	# date created DateField with a default of today's date
+	date_created = models.DateField('date created', default=date.today)
+
+	
