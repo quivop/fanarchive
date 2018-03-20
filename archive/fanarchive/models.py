0506549e671b21bba8c08fccc115a36fc6bd0dad
quivop <37252176+quivop@users.noreply.github.com>
2018-03-19 18:27:07 -0700

    Make sure Work model returns its title when called by __str__()

--- archive/fanarchive/models.py
+++ archive/fanarchive/models.py
@@ -10,4 +10,6 @@ class Work(models.model):
 	# date created DateField with a default of today's date
 	date_created = models.DateField('date created', default=date.today)
 
-	
+	def __str__(self):
+		# returning a nice, human-readable representation of the model when called by __str__() method
+		return self.work_title
