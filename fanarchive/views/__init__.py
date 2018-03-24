3fc7fabdde7db448d6884affd0558d3f4f457428
quivop <37252176+quivop@users.noreply.github.com>
2018-03-24 13:16:58 -0700

    Break views into separate files

---
+++ fanarchive/views/__init__.py
@@ -0,0 +1,2 @@
+from .index import *
+from .detail import *
---
+++ fanarchive/views/index.py
@@ -0,0 +1,11 @@
+from django.views import generic
+
+from fanarchive.models import Work, WorkPart
+
+class IndexView(generic.ListView):
+	template_name = 'fanarchive/index.jinja'
+	context_object_name = 'latest_work_list'
+
+	def get_queryset(self):
+		"""Return the last five published works"""
+		return Work.objects.order_by('-date_created')[:5]
