eded821ffe17a18f215abe2d89559edd327b13bb
quivop <37252176+quivop@users.noreply.github.com>
2018-03-24 13:38:51 -0700

    Remove superfluous comments and imports

--- fanarchive/models/work.py
+++ fanarchive/models/work.py
@@ -1,19 +1,13 @@
 from django.db import models
 from datetime import date
 
-# Work model
-
 class Work(models.Model):
-	# work title CharField with generous length allowance
 	work_title = models.CharField('work title', max_length=200)
-	# work summary TextField with max-length enforced only at view level, not on db or in the model
+	# remember, this is enforced only at view level, not on db or in the model. needs testing
 	work_summary = models.TextField('work summary', max_length=500)
-	# date created DateField with a default of today's date
+	# default to today
 	date_created = models.DateField('date created', default=date.today)
 
 	def __str__(self):
-		# returning a nice, human-readable representation of the model when called by __str__() method
+		# returns title of the Work when called by __str__() method
 		return self.work_title
-
-	# class Meta:
-	# 	app_label = 'fanarchive'
