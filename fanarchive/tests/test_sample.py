d1120326c6c7351987a5292a0a580616589f1f1d
Quivo <37252176+quivop@users.noreply.github.com>
2018-03-24 19:19:30 -0700

    Empty init and rename test files so they are picked up

--- fanarchive/tests/test_sample.py
+++ fanarchive/tests/test_sample.py
@@ -1,9 +1,7 @@
 from django.test import TestCase
 
-# Create your tests here.
-
 class SampleUnitTests(TestCase):
 
-	def sample_friggin_test(self):
+	def test_sample_friggin_test(self):
 		self.assertEquals(1+1, 2)
 
