7bee2d51d398bb1a898321bc18c9e6565da89a22
Quivo <37252176+quivop@users.noreply.github.com>
2018-03-24 19:49:20 -0700

    Add field label tests for Work model
    Also remove unneeded sample model test.

--- fanarchive/tests/test_models.py
+++ fanarchive/tests/test_models.py
@@ -1,7 +1,27 @@
 from django.test import TestCase
 
-class SampleModelTests(TestCase):
+from fanarchive.models import Work
+from django.utils import timezone
 
-	def test_sample_friggin_test_2(self):
-		self.assertEquals(1+3, 4)
+class WorkModelTest(TestCase):
 
+	@classmethod
+	def setUpTestData(cls):
+		# Set up non-modified objects used by all test methods
+		Work.objects.create(work_title='Ermagerd', work_summary="Really, erma freaking gard, they're sleeping together...", date_created=timezone.now())
+
+	# testing field labels~
+	def test_work_title_label(self):
+		work=Work.objects.get(id=1)
+		field_label = work._meta.get_field('work_title').verbose_name
+		self.assertEquals(field_label, 'work title')
+
+	def test_work_summary_label(self):
+		work=Work.objects.get(id=1)
+		field_label = work._meta.get_field('work_summary').verbose_name
+		self.assertEquals(field_label, 'work summary')
+
+	def test_date_created_label(self):
+		work=Work.objects.get(id=1)
+		field_label = work._meta.get_field('date_created').verbose_name
+		self.assertEquals(field_label, 'date created')
