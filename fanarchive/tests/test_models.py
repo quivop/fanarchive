fe633f3bb9f964105524d20f52b506ed93cacd62
Quivo <37252176+quivop@users.noreply.github.com>
2018-03-24 20:20:23 -0700

    Add field label test and date_created test

--- fanarchive/tests/test_models.py
+++ fanarchive/tests/test_models.py
@@ -1,14 +1,15 @@
 from django.test import TestCase
 
 from fanarchive.models import Work
-from django.utils import timezone
+from datetime import date
+import datetime
 
 class WorkModelTest(TestCase):
 
 	@classmethod
 	def setUpTestData(cls):
 		# Set up non-modified objects used by all test methods
-		Work.objects.create(work_title='Ermagerd', work_summary="Really, erma freaking gard, they're sleeping together...", date_created=timezone.now())
+		Work.objects.create(work_title='Ermagerd', work_summary="Really, erma freaking gard, they're sleeping together...",)
 
 	# testing field labels~
 	def test_work_title_label(self):
@@ -25,3 +26,14 @@ class WorkModelTest(TestCase):
 		work=Work.objects.get(id=1)
 		field_label = work._meta.get_field('date_created').verbose_name
 		self.assertEquals(field_label, 'date created')
+
+	def test_object_name_is_work_title(self):
+		work=Work.objects.get(id=1)
+		expected_object_name = '%s' % (work.work_title)
+		self.assertEquals(expected_object_name,str(work))
+
+	def test_date_created_is_today(self):
+		# also checks that a default date is being set at all
+		work=Work.objects.get(id=1)
+		expected_date = date.today()
+		self.assertEquals(expected_date, work.date_created)	
