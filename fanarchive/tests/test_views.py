74a0db3b6e49df913b80f2ee2b8f3a5e750a63e4
quivop <37252176+quivop@users.noreply.github.com>
2018-03-30 15:44:18 -0700

    Add test for homepage redirect to fanarchive app

---
+++ fanarchive/tests/test_views.py
@@ -0,0 +1,9 @@
+from django.test import TestCase
+
+
+class IndexRedirectTest(TestCase):
+	def test_if_site_homepage_redirects_to_fanarchive(self):
+		'''Going to 'archive.homepage/' redirects you to the '/fanarchive/' app directory.
+		'''
+		response = self.client.get('/')
+		self.assertRedirects(response, expected_url="/fanarchive/", status_code=302, target_status_code=200, fetch_redirect_response=True)
