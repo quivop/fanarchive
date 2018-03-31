edf160fb398dd590a8932e5da5e287f01b33398d
quivop <37252176+quivop@users.noreply.github.com>
2018-03-31 16:09:49 -0700

    Add tests for WorkPart model

--- fanarchive/tests/test_models.py
+++ fanarchive/tests/test_models.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 
-from fanarchive.models import Work
+from fanarchive.models import Work, WorkPart
 from datetime import date, timedelta
 
 
@@ -9,10 +9,15 @@ class WorkModelTest(TestCase):
 	@classmethod
 	def setUpTestData(cls):
 		# Set up non-modified objects used by all test methods
+		# 
+		# Work with default date_created (work1)
 		Work.objects.create(work_title='Ermagerd', work_summary="Really, erma freaking gard, they're sleeping together...",)
-		Work.objects.create(work_title='No really', work_summary="What in the ACTUAL HELLO", date_created=(date.today() - timedelta(days=1)))
+		# Work with future date_created (work2)
+		Work.objects.create(work_title='No really', work_summary="What in the ACTUAL HELLO", date_created=(date.today() + timedelta(days=1)))
+		# Work part related to work1 (work_part1)
+		WorkPart.objects.create(work_id=1, work_part_title='Fits around me so tight', work_part_text='No-o...') 
 
-	# testing field labels~
+	# Work Tests
 	def test_work_title_label(self):
 		work=Work.objects.get(id=1)
 		field_label = work._meta.get_field('work_title').verbose_name
@@ -42,6 +47,35 @@ class WorkModelTest(TestCase):
 	def test_work_title_max_length(self):
 		work=Work.objects.get(id=1)
 		max_length = work._meta.get_field('work_title').max_length
-		self.assertEquals(max_length,200)
+		self.assertEquals(max_length, 200)
+
+	# WorkPart tests
+	def test_work_part_title_label(self):
+		work_part=WorkPart.objects.get(id=1)
+		field_label= work_part._meta.get_field('work_part_title').verbose_name
+		self.assertEquals(field_label, 'work part title')
+
+	def test_work_part_text_label(self):
+		work_part=WorkPart.objects.get(id=1)
+		field_label = work_part._meta.get_field('work_part_text').verbose_name
+		self.assertEquals(field_label, 'work part text')
+
+	def test_work_part_title_max_length(self):
+		work_part=WorkPart.objects.get(id=1)
+		max_length = work_part._meta.get_field('work_part_title').max_length
+		self.assertEquals(max_length, 200)
+
+	def test_work_part_is_related_to_correct_work(self):
+		work_part=WorkPart.objects.get(id=1)
+		work_id = work_part.work_id
+		self.assertEquals(work_id, 1)
+
+
+# FUTURE TESTS #
+
+# test that work with future date_created posts properly
+# test that work with past date_created posts properly
+
+# test that works are sorted by date in reverse order?
 
 
