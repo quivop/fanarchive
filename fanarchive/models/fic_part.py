b251eb42da06b04583dc0b710f8011be60515e30
quivop <37252176+quivop@users.noreply.github.com>
2018-04-07 02:31:57 -0700

    Change models to fic-specific ones

---
+++ fanarchive/models/fic_part.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class FicPart(models.Model):
+    fic_part_title = models.CharField('fic part title', max_length=200)
+    fic_part_text = models.TextField('fic part text')
+
+    # One FicPart can have many Fics, but a FicPart can only have one Fic
+    fic = models.ForeignKey('Fic', on_delete=models.CASCADE)
+
+    def __str__(self):
+        # returns title of the FicPart when called by __str__()
+        return self.fic_part_title
