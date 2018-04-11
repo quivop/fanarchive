0cf7161cf156467e9fe021cb52f9991191972764
Quivo <37252176+quivop@users.noreply.github.com>
2018-04-11 13:45:20 -0700

    Clean up script
    Remove superfluous comments
    Leave the SECRET_KEY alone if it's already set
    Add heroku formatting to  messages

--- .release-tasks.sh
+++ .release-tasks.sh
@@ -1,29 +1,20 @@
 #!/bin/bash
 
-# set environment variables
+# set debug environment variable
 DEBUG=False
-
-# SECRET_KEY="fakekey" # set because Django whines if you don't 
 
 # define secret key function
 function mk_secret_key () {
 	python manage.py shell -c "from django.core.management import utils; print(utils.get_random_secret_key())"
 } 
 
+# If a secret key exists...
+if [ -z "$SECRET_KEY"] ; then
+	echo `SECRET_KEY=$(mk_secret_key)`
 # Make a new key and stuff it into heroku's mouth:
-echo `SECRET_KEY=$(mk_secret_key)`
+else
+	echo "-----> Secret key already set. Moving on..."
 
 # run database migrations
-echo "Running db migrations..."
+echo "-----> Running db migrations..."
 echo `python manage.py migrate`
-
-# notify user re how to load data
-# "To load sample data into the app, type './.loaddata' into your shell, and press enter."
-
-# # load test data from fixtures
-# echo -e "Do you want to load test data into the archive?\\n
-# Type 'y' for yes, or 'n' for no: \c"
-# read answer
-
-# if [[ $answer -eq 'y' ]] ; then
-# 	echo `python manage.py loaddata fanarchive\fixtures\initial_data.json`
