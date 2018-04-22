46c8d409f2c547c9eb6fffacea97763165676e13
Quivo <37252176+quivop@users.noreply.github.com>
2018-04-22 12:55:03 -0700

    Add 404 and 500 pages (#22)
    * Add rough templates for 404 and 500 error pages
    
    * Change debug to false
    (heroku-specific issue I was trying to test)
    
    * Reset debug to false
    (sorted out heroku-specific issue and reset DEBUG to old value)
    
    * Make homepage link point to homepage
    
    * Add tests for 404 and 500 error views
    
    * Change 500 error test to stub

--- fanarchive/tests/test_views.py
+++ fanarchive/tests/test_views.py
@@ -1,9 +1,10 @@
+from datetime import timedelta
 from django.test import TestCase
 from django.urls import reverse
+from django.utils import timezone
+from django.http import HttpResponseServerError
 
 from fanarchive.models import Fic, FicPart
-from datetime import timedelta
-from django.utils import timezone
 
 
 class IndexViewTest(TestCase):
@@ -65,4 +66,27 @@ class DetailViewTest(TestCase):
         pass
 
     def test_detail_view_displays_fic_and_fic_part(self):
+        pass
+
+
+class ErrorViewTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        # create a bunch of fics and fic parts
+        number_of_fics = 3
+        for fic_num in range(number_of_fics):
+            Fic.objects.create(
+                fic_title='Fic %s' % fic_num,
+                fic_summary='Butts',
+                pub_date=(timezone.now() - timedelta(days=fic_num)))
+
+    def test_if_404_error_is_handled_correctly(self):
+        resp = self.client.get('/yay_404')
+        self.assertEqual(resp.status_code, 404)
+        self.assertTemplateUsed(resp, '404.html')
+
+    def test_if_500_error_is_handled_correctly(self):
+        # leaving this a stub test because it needs selenium to function correctly.
+
         pass
