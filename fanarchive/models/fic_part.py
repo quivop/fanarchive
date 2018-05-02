899a2f1ac97b73b7ee22532d05fbdf934f2e968a
Quivo <37252176+quivop@users.noreply.github.com>
2018-05-02 00:45:51 -0700

    Add partially working save method and part # calculator
    - custom save method that tries and fails to validate fields properly
    - fic_part_number calculator method that mostly works
    	- calculator method ignores duplicate fic_part_numbers that are lower than the total number of FicParts assigned to the Fic in question

--- fanarchive/models/fic_part.py
+++ fanarchive/models/fic_part.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 
 
 class FicPart(models.Model):
@@ -15,3 +16,64 @@ class FicPart(models.Model):
     def __str__(self):
         # returns title of the FicPart when called by __str__()
         return self.fic_part_title
+
+    def Meta(self):
+        unique_together = ("fic", "fic_part_number") # noqa
+
+    def save(self, *args, **kwargs):
+        if self.fic_part_number is None:
+            fic_part_number = calc_fic_part_number(
+                fic_id=self.fic)
+        else:
+            fic_part_number = calc_fic_part_number(
+                fic_part=self, fic_id=self.fic,
+                fic_part_number=self.fic_part_number)
+        self.fic_part_number = fic_part_number
+
+        try:
+            self.full_clean()
+        except ValidationError as e:
+            self.fic_part_number = None
+            self.fic_part_number = calc_fic_part_number(
+                fic_id=self.fic)
+        else:
+            super(FicPart, self).save(*args, **kwargs)
+
+
+def calc_fic_part_number(fic_id, fic_part=None,
+                         fic_part_number=None):
+    # grab existing fic part numbers for the Fic of this FicPart
+    existing_part_numbers = FicPart.objects.filter(
+        fic=fic_id)
+    # count how many existing fic parts there are in the set
+    count = existing_part_numbers.count()
+
+    # format the existing fic parts...
+    ordered_parts = existing_part_numbers.order_by(
+        '-fic_part_number')
+    # ...and extract their part numbers
+    part_list = ordered_parts.values_list(
+        'fic_part_number', flat=True)
+
+    if fic_part_number is None and part_list[0] is None:
+        return 1
+    elif ((fic_part_number == 0) or (fic_part_number == 1)) and count == 1:
+        return 1
+    elif fic_part_number >= count:
+        try:
+            fic_part.fic_part_number = fic_part_number
+            fic_part.validate_unique()
+        except ValidationError as e:
+            return count+1
+        else:
+            return count
+    elif fic_part_number < count:
+        try:
+            fic_part.fic_part_number = fic_part_number
+            fic_part.validate_unique()
+        except ValidationError as e:
+            raise ValidationError('\
+                The FicPart number is not unique. Please\
+                change it to a unique number.')
+        else:
+            return fic_part_number
