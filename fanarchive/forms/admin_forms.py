7cb145047ba5854a0b8700cf290044d20b551dd2
Quivo <37252176+quivop@users.noreply.github.com>
2018-05-19 15:15:53 -0700

    Add super-simple fic editing form (#25)
    * Add super-simple fic-editing form
    
    * Add stub view for editing fics
    
    * Correct lint error
    
    * Make sure stub fic-editing view works
    
    * Make fic-editing form work
    
    - Add shitty template for fic-editing form
    - For successful edits, redirect to index page
    - Edit url file to work with expanded FicEditingView

---
+++ fanarchive/forms/admin_forms.py
@@ -0,0 +1,16 @@
+from django import forms
+from django.core.exceptions import ValidationError
+from django.utils.translation import ugettext_lazy as _
+
+
+class EditFicForm(forms.Form):
+    fic_title = forms.CharField(help_text="Enter a shiny new title for your fanfic (default is original title).")
+
+    def clean_fic_title(self):
+        data = self.cleaned_data['fic_title']
+
+        # Check title is not longer than 200 characters
+        if len(data) > 200:
+            raise ValidationError(_('Title is too long - fic title must be 200 characters or less in length'))
+
+        return data
