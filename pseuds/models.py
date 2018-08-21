6a4d1499d2fd05ac88c8c8151866ac45ffb3cbac
Quivo <37252176+quivop@users.noreply.github.com>
2018-08-21 16:29:50 -0700

    Remove extra argument in pseud_owner definition

--- pseuds/models.py
+++ pseuds/models.py
@@ -7,6 +7,6 @@ from django.db import models
 class Pseud(models.Model):
     pseud_name = models.CharField('pseud name', max_length=100)
     # needs enforcing @ db level
-    pseud_owner = models.ForeignKey('pseud owner',
+    pseud_owner = models.ForeignKey(
                                     settings.AUTH_USER_MODEL,
                                     on_delete=models.CASCADE)
