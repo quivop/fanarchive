57767f058f45c342aeb164ec202f462a11229adb
Quivo <37252176+quivop@users.noreply.github.com>
2018-08-21 18:46:33 -0700

    rename author models file

--- fanfic/models/__init__.py
+++ fanfic/models/__init__.py
@@ -1,3 +1,3 @@
 from .fic import Fic  # noqa: F401
 from .fic_part import FicPart  # noqa: F401
-from .misc import AuthorGroup, Authorship # noqa :F401
+from .author_related import AuthorGroup, Authorship # noqa :F401
