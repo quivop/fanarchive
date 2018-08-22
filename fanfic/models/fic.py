46bccdb48f66ad3f307c6762dee5e041dde859b3
Quivo <37252176+quivop@users.noreply.github.com>
2018-08-21 19:05:11 -0700

    Add docstrings + refactor

--- fanfic/models/fic.py
+++ fanfic/models/fic.py
@@ -3,29 +3,38 @@ from django.utils import timezone
 
 
 class Fic(models.Model):
+    """
+    Stores a single fanfic, related to :model:`fic_part.FicPart`
+    and :model:`author_related.AuthorGroup`.
+    """
+
+    # max_length only enforced at view level
     fic_title = models.CharField('fic title', max_length=200)
-    # remember, this is enforced only at view level, not on db or in the model. needs testing
     fic_summary = models.TextField('fic summary', max_length=500)
+
     # default both date fields to now
     pub_date = models.DateTimeField('date published', default=timezone.now)
     date_updated = models.DateTimeField('date updated', default=timezone.now)
-    fic_author_group = models.ForeignKey('AuthorGroup', on_delete=models.CASCADE)
+
+    fic_author_group = models.ForeignKey(
+        'AuthorGroup', on_delete=models.CASCADE,
+        verbose_name='fic author(s)')
 
     def __str__(self):
         """
-        String for representing the Model object
+        Human-readable representation of a :model:`fic.Fic`
         """
         return self.fic_title
 
     def get_absolute_url(self):
         """
-        Returns the url to access the detail page for this fanfic
+        Returns the url to access the detail page for a Fic.
         """
         from django.urls import reverse
         return reverse('fanfic:detail', args=[str(self.id)])
 
     def get_fic_authors(self):
         """
-        Returns a list of the fic author(s)
+        Returns an alphabetized list of the fic author(s) for a Fic.
         """
         pass
