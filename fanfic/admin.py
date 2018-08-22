73e83851c60ce913442ce2f686c007cf95c9533d
Quivo <37252176+quivop@users.noreply.github.com>
2018-08-21 19:34:03 -0700

    Register new fanfic models

--- fanfic/admin.py
+++ fanfic/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 
 # registering our models
-from .models import Fic, FicPart
+from .models import Fic, FicPart, AuthorGroup, Authorship, Pseud
 
 admin.site.register(Fic)
 admin.site.register(FicPart)
+admin.site.register(AuthorGroup)
+admin.site.register(Authorship)
+admin.site.register(Pseud)
