2508f3f90deadf127b282f0df8d9280930aad28d
Quivo <37252176+quivop@users.noreply.github.com>
2018-08-25 15:07:29 -0700

    Remove PseudGroup and PseudInstance models

--- fanfic/models/__init__.py
+++ fanfic/models/__init__.py
@@ -1,4 +1,4 @@
 from .fic import Fic  # noqa: F401
 from .fic_part import FicPart  # noqa: F401
 from .authors import AuthorGroup, Authorship # noqa :F401
-from .pseud import Pseud, PseudGroup, PseudInstance  # noqa: F401
+from .pseud import Pseud  # noqa: F401
