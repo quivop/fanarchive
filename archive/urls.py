de6988cf2a130b6be520f07b9f22a179af380ed6
Quivo <37252176+quivop@users.noreply.github.com>
2018-08-30 23:15:49 -0700

    Remove django admin from urls

--- archive/urls.py
+++ archive/urls.py
@@ -2,7 +2,7 @@
 archive URL Configuration
 
 """
-from django.contrib import admin
+# from django.contrib import admin
 from django.urls import path, include  # we need include, so add it here
 
 # Import RedirectView so redirecting our base url
@@ -10,7 +10,7 @@ from django.urls import path, include  # we need include, so add it here
 from django.views.generic import RedirectView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
 
     # forward requests for 'archive/' links to urls.py
     # in the fanfic app
