67f5263d3ca420e42e4e2af34c1fc536dba1b5cb
quivop <37252176+quivop@users.noreply.github.com>
2018-12-01 01:39:39 -0800

    Move and rename login route to the users app

--- users/urls.py
+++ users/urls.py
@@ -1,1 +1,8 @@
-# urls here
+from django.urls import path, include
+from authtools.views import LoginView as AuthToolsLoginView
+from django.views.generic import RedirectView
+
+urlpatterns = [
+    path('login/', AuthToolsLoginView.as_view(template_name='users/login.html'), name='login'),
+    path('', RedirectView.as_view(url='login/'))
+]
