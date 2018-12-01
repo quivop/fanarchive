509ab5ce215322f8d5b36c4037a9897614b1ca15
quivop <37252176+quivop@users.noreply.github.com>
2018-12-01 02:43:03 -0800

    Add signup view and route

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from authtools.views import LoginView, LogoutView, PasswordChangeView, PasswordChangeDoneView, PasswordResetView, PasswordResetDoneView, PasswordResetConfirmView, PasswordResetCompleteView
+from . import views
 
 from django.views.generic import RedirectView
 
@@ -8,6 +9,9 @@ urlpatterns = [
     path('login/', LoginView.as_view(template_name='users/login.html'), name='login'),
     path('', RedirectView.as_view(url='login/')),
     path('logout/', LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('signup/', views.SignUp.as_view(), name='signup'),
+
+    # routes with un-implemented templates below here
     path('password_change/', PasswordChangeView.as_view(template_name='users/password_change.html')),
     path('password_change/done/', PasswordChangeDoneView.as_view(template_name='users/password_change_done.html')),
     path('password_reset/', PasswordResetView.as_view(template_name='users/password_reset.html')),
