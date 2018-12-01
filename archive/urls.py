30687e7777fbdcecf830cc2c00bef02f5ea03f28
quivop <37252176+quivop@users.noreply.github.com>
2018-12-01 01:31:31 -0800

    Add route for users app

--- archive/urls.py
+++ archive/urls.py
@@ -8,4 +8,7 @@ from django.urls import path, include  # we need include, so add it here
 urlpatterns = [
     # forward requests for root links to fanfic app
     path('', include('fanfic.urls', namespace='fanfic')),
+
+    # set up routing for users app
+    path('users/', include('users.urls'))
 ]
