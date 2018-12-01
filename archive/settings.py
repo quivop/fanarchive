a801426d10341ef7129961708258754803c30131
quivop <37252176+quivop@users.noreply.github.com>
2018-11-30 19:03:16 -0800

    Change custom user model name, and add authtools forms and a urls file

--- archive/settings.py
+++ archive/settings.py
@@ -145,7 +145,7 @@ DATABASES = {
 
 # Custom user model
 
-AUTH_USER_MODEL = 'users.User'
+AUTH_USER_MODEL = 'users.FicUser'
 
 # Password validation
 
--- fanfic/urls.py
+++ fanfic/urls.py
@@ -13,5 +13,5 @@ urlpatterns = [
     path('test/', views.TestView.as_view(), name='test'),
     # path('admin/<int:pk>/edit/', views.FicEditingView, name='fic-editing-view'),
     path('panel/', views.MyPanel.as_view(), name='panel'),
-    path('auth/login/', AuthToolsLoginView, name='login')
+    path('auth/login/', AuthToolsLoginView, name='login'),
 ]
