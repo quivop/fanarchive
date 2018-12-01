8fd0430f37a8ab178809a9b8acb6f06fe3a8a786
quivop <37252176+quivop@users.noreply.github.com>
2018-12-01 01:30:20 -0800

    Clean up old comments and unneeded code

--- archive/urls.py
+++ archive/urls.py
@@ -5,17 +5,7 @@ archive URL Configuration
 # from django.contrib import admin
 from django.urls import path, include  # we need include, so add it here
 
-# Import RedirectView so redirecting our base url
-# to the fanfic app will work
-from django.views.generic import RedirectView
-
 urlpatterns = [
-    # path('admin/', admin.site.urls),
-
-    # forward requests for 'archive/' links to urls.py
-    # in the fanfic app
+    # forward requests for root links to fanfic app
     path('', include('fanfic.urls', namespace='fanfic')),
-
-    # redirect the base url to the fanfic app
-    path('', RedirectView.as_view(url='archive/')),
 ]
