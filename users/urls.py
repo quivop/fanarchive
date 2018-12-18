16285928f63123da7e781635da5135afaa2f6db0
Quivo <37252176+quivop@users.noreply.github.com>
2018-12-17 20:53:17 -0800

    Reorder and tidy up the file

--- users/urls.py
+++ users/urls.py
@@ -6,18 +6,20 @@ from django.views.generic import RedirectView
 
 app_name = "users"
 urlpatterns = [
+
+    # login/logout
     path('login/', LoginView.as_view(template_name='users/login.html'), name='login'),
     path('', RedirectView.as_view(url='login/')),
     path('logout/', LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('signup/', views.SignUp.as_view(), name='signup'),
-    path('password_reset/', views.ResetView.as_view(), name='password_reset'),
-    path('password_reset/done/', PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'), name='password_reset_done'),
 
-    # routes with un-implemented templates below here
+    # change password
     path('password_change/', views.PasswordChange.as_view(), name='password_change'),
     path('password_change/done/', PasswordChangeDoneView.as_view(template_name='users/password_change_done.html'), name='password_change_done'),
     
-    
+    # reset password
+    path('password_reset/', views.ResetView.as_view(), name='password_reset'),
+    path('password_reset/done/', PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', views.ResetConfirmView.as_view(), name='password_reset_confirm'),
     path('reset/done/', PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'), name='password_reset_complete'),
 ]
