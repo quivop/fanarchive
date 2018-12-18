63a87d7b9255edb2c5139bc59f7db39f42f3357f
Quivo <37252176+quivop@users.noreply.github.com>
2018-12-17 19:52:21 -0800

    Add names to remaining paths

--- users/urls.py
+++ users/urls.py
@@ -14,10 +14,10 @@ urlpatterns = [
     path('password_reset/done/', PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'), name='password_reset_done'),
 
     # routes with un-implemented templates below here
-    path('password_change/', PasswordChangeView.as_view(template_name='users/password_change.html')),
-    path('password_change/done/', PasswordChangeDoneView.as_view(template_name='users/password_change_done.html')),
+    path('password_change/', PasswordChangeView.as_view(template_name='users/password_change.html'), name='password_change'),
+    path('password_change/done/', PasswordChangeDoneView.as_view(template_name='users/password_change_done.html'), name='password_change_done'),
     
     
-    path('reset/<uidb64>/<token>/', PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html')),
-    path('reset/done/', PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html')),
+    path('reset/<uidb64>/<token>/', PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('reset/done/', PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'), name='password_reset_complete'),
 ]
