f01e077538aea9059cd95b777e2f30aa44782dcc
Quivo <37252176+quivop@users.noreply.github.com>
2018-12-17 20:48:16 -0800

    Make password change cycle work

--- users/urls.py
+++ users/urls.py
@@ -14,7 +14,7 @@ urlpatterns = [
     path('password_reset/done/', PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'), name='password_reset_done'),
 
     # routes with un-implemented templates below here
-    path('password_change/', PasswordChangeView.as_view(template_name='users/password_change.html'), name='password_change'),
+    path('password_change/', views.PasswordChange.as_view(), name='password_change'),
     path('password_change/done/', PasswordChangeDoneView.as_view(template_name='users/password_change_done.html'), name='password_change_done'),
     
     
--- users/views.py
+++ users/views.py
@@ -1,13 +1,18 @@
 from .forms import FicUserCreationForm
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
-from authtools.views import PasswordResetView, PasswordResetConfirmView
+from authtools.views import PasswordChangeView, PasswordResetView, PasswordResetConfirmView
 
 
 class SignUp(CreateView):
     form_class = FicUserCreationForm
     success_url = reverse_lazy('users:login')
     template_name = 'users/signup.html'
+
+
+class PasswordChange(PasswordChangeView):
+    template_name = 'users/password_change.html'
+    success_url = reverse_lazy('users:password_change_done')
 
 
 class ResetView(PasswordResetView):
